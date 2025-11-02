6a36fe285eb47490450c8a73797e88a610fbb271
Alfee Khan <alfeekhan20@gmail.com>
2025-11-02 20:32:57 +0530

    Initial commit for Arrays II

---
+++ Assignments/AssOOPS.cpp
@@ -0,0 +1,22 @@
+// //QUESTION 2
+// #include <iostream>
+// using namespace std;
+// class BankAccount{
+//      int accountNumber;
+//      int balance;
+
+//      public:
+//      void deposit(){
+//         cout<<"amount deposited\n";
+//      }
+//      void withdraw(){
+//         cout<<"amount withdraw\n";
+//      }
+//      void getBalance(){
+//         cout<<"amount left\n";
+//      }
+// }
+// int main() {
+   
+//     return 0;
+// }
