7ffcddaf9969fddf6d24b925b7b5af9235fab2e1
Alfee Khan <alfeekhan20@gmail.com>
2025-11-04 23:17:33 +0530

    Update assignments and recursion files: add Ass10(STR) & Ass9(2D), remove old files, update recursion binaries/sources

---
+++ Assignments/Ass10(STR).cpp
@@ -0,0 +1,51 @@
+//QUES 1
+#include <iostream>
+using namespace std;
+
+int vowelCounter(string str)
+{
+    int count = 0;
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' ||  str[i] == 'o' || str[i] == 'u')
+        {
+            count++;
+          
+        }
+  
+      
+    }
+            cout << " vowel occurs : " << count<<endl ;
+   
+}
+
+// QUES 2
+bool swapWord (string s1 , string s2){
+    if(s1.length() != s2.length()){
+        return false;
+    }
+    for (int i = 0; i < s1.length(); i++)
+    {
+       swap(s1[i], s1[i]);
+    }
+    
+}
+    int main()
+    {
+        string str = "Alfeekhan";
+        // string str = "america";
+        vowelCounter(str);
+
+
+           string s1 = "bank";
+           string s2 =  "kanb";
+
+   swapWord(s1 , s2);
+        return 0;
+    }
+
+
+
+
+//q3 = 1657 LEETCODE
+//q4 = 20 LEETCODE
--- CPP CODES/Recursion/factorial.cpp
+++ CPP CODES/Recursion/factorial.cpp
@@ -12,44 +12,34 @@ int factorial(int n)
 
     return n * factorial(n - 1);
 }
+
+void func(int n)
+{
+    if (n == 0)
+    { // base case
+        return;
+    }
+
+    //   cout<<n<<" ";
+    //   func(n-1); descending order
+
+    func(n - 1);
+    cout << n << " "; // ascending order
+}
+// work = cout n ko
+// assume = print n -1
+// base case => n = 0 return kar denge
 int main()
 {
     int ans = factorial(4);
-    cout<<ans<<endl;
+    cout << ans << endl;
+
+    // func(5);
     return 0;
 }
 
 // Recursion = it is a method of solving computational problems where the solution depends on solutions
-// to smaller instances of the same problem 
-// 1 Base case /smallest problem 
-// 2 in each function call ? kya work karwaana hai 
-// 3 how to call inner function 
-
-
-#include <iostream>
-using namespace std;
-
-
-void func(int n){
-    if(n==0){  // base case 
-        return ;
-    }
-  
-
-    //   cout<<n<<" ";
-    //   func(n-1); descending order 
-    
-
-  
-      func(n-1);
-          cout<<n<<" "; // ascending order 
-    
-}
-int main() {
-   func(5);
-    return 0;
-}
-
-// work = cout n ko 
-// assume = print n -1 
-// base case => n = 0 return kar denge 
+// to smaller instances of the same problem
+// 1 Base case /smallest problem
+// 2 in each function call ? kya work karwaana hai
+// 3 how to call inner function
