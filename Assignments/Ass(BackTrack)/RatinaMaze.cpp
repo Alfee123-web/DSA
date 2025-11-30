16bbd0a2ba2789298a83c4b8c0a4543f793217ad
Alfee Khan <alfeekhan20@gmail.com>
2025-11-30 22:39:31 +0530

    Assignment files added

---
+++ Assignments/Ass(BackTrack)/RatinaMaze.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void helper(vector<vector<int>> &mat, int r, int c, string path, vector<string> &ans, vector<vector<bool>> vis)
+{
+    int n = mat.size();
+    // Base case
+    if (r < 0 || c < 0 || r >= n || c >= n || mat[r][c] == 0 || vis[r][c] == true)
+    {
+        return;
+    }
+    if (r == n - 1 && c == n - 1)
+    {
+        ans.push_back(path);
+        return;
+    }
+    vis[r][c] = true;
+
+    helper(mat, r - 1, c, path + "U", ans, vis); // UP
+    helper(mat, r + 1, c, path + "D", ans, vis); // DOWN
+    helper(mat, r, c - 1, path + "L", ans, vis); // LEFT
+    helper(mat, r, c + 1, path + "R", ans, vis); // RIGHT
+    vis[r][c] = false;
+}
+vector<string> findPath(vector<vector<int>> &mat)
+{
+    int n = mat.size();
+    vector<string> ans;
+    string path = "";
+    vector<vector<bool>> vis(n, vector<bool>(n, false));
+
+    helper(mat, 0, 0, path, ans, vis);
+    return ans;
+}
+
+int main()
+{
+
+    vector<vector<int>> mat = {
+        {1, 0, 0, 0},
+        {1, 1, 0, 1},
+        {1, 1, 0, 0},
+        {0, 1, 1, 1}};
+    vector<string> ans = findPath(mat);
+    for (const string &p : ans)
+    {
+        cout << p << endl;
+    }
+
+    return 0;
+}
+// OUTPUT
+
+// DDRDRR
+// DRDDRR
