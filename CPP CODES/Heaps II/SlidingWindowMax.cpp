ba5e978f43623ed286975c9c4a7d3e4fc5257ece
Alfee Khan <alfeekhan20@gmail.com>
2026-03-15 22:08:17 +0530

    HEAPS files added

--- CPP CODES/Heaps II/SlidingWindowMax.cpp
+++ CPP CODES/Heaps II/SlidingWindowMax.cpp
@@ -3,8 +3,40 @@
 #include <queue>
 #include <vector>
 using namespace std;
+void slidingWindowMax(vector<int> arr, int K)
+{
+    priority_queue<pair<int, int>> pq;
 
-int main() {
-   
+    // pq.top()=>sliding window part ? =>  el val , el idx
+    // pairs ( arr[i] , i ) => by default val(arr[i]) basis par heap build hojayega and
+    // comparison will be on the basis of idx i
+    //  store both values val , idx
+
+    // 1st window
+    for (int i = 0; i < K; i++)
+    {
+        pq.push(make_pair(arr[i], i));
+    }
+    cout << "output : " << pq.top().first << " ";
+
+    for (int i = K; i < arr.size(); i++)
+    {
+        while (!pq.empty() && pq.top().second <= i - K)
+        {
+            // outside window
+            pq.pop();
+        }
+        pq.push(make_pair(arr[i], i));
+        cout << pq.top().first << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+    vector<int> arr = {1, 3, -1, -3, 5, 3, 6, 7};
+    int K = 3;
+    slidingWindowMax(arr, K);
+
     return 0;
 }
+// output : 3 3 5 5 6 7
